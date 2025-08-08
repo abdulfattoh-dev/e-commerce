111e1e8b012204a4f5b81e93dc5e96fc587e22af
abdulfattoh-dev <abdulfattoh.dev@gmail.com>
2025-08-08 19:18:50 +0500

    full with media

--- src/pages/product/ProductDetail.tsx
+++ src/pages/product/ProductDetail.tsx
@@ -10,9 +10,9 @@ const ProductDetail = () => {
   return (
     <div>
       <div className="container">
-        <div className="flex justify-between mt-25.5">
-          <div className="w-[640px] flex gap-[41px] items-center border-1 border-[#9F9F9F]">
-            <div className="w-[58px] my-12 ml-6.5 flex flex-col gap-[17px]">
+        <div className="flex justify-between mt-25.5 max-lg:flex-col">
+          <div className="w-[49%] max-lg:w-full flex max-lg:flex-col-reverse gap-[41px] max-sm:gap-[28px] items-center border-1 border-[#9F9F9F]">
+            <div className="w-[58px] max-sm:w-[262px] my-12 ml-6.5 flex flex-col max-lg:w-full max-lg:flex-row max-lg:justify-center gap-[17px] max-lg:gap-15 max-sm:gap-[10px]">
               <div>
                 <img src={productSwiper} alt="" />
               </div>
@@ -29,14 +29,14 @@ const ProductDetail = () => {
                 <img src={productSwiper} alt="" />
               </div>
             </div>
-            <div className="w-[429px] my-[30.5px] mr-21.5">
+            <div className="w-[429px] max-sm:w-[175px] my-[30.5px] max-sm:mb-0 mr-21.5 max-lg:mr-0">
               <img src={productDetailImage} alt="" />
             </div>
           </div>
-          <div className="w-[600px]">
+          <div className="w-[45%] max-lg:w-full max-lg:mt-8">
             <h2 className="text-5xl font-bold">Встраиваемый светильник Novotech</h2>
-            <div className="flex justify-between mt-9.5 pb-10 border-b-1 border-b-[#9F9F9F]">
-              <div className="flex gap-8">
+            <div className="flex max-sm:flex-col max-sm:gap-12 justify-between mt-9.5 pb-10 border-b-1 border-b-[#9F9F9F]">
+              <div className="flex gap-8 max-sm:justify-between">
                 <div>
                   <del className="text-xs text-[#9F9F9F]">7 000₽</del>
                   <p className="text-xl font-bold">6 399₽</p>
@@ -57,8 +57,8 @@ const ProductDetail = () => {
                 </button>
               </div>
             </div>
-            <div className="flex justify-between">
-              <div className="w-[271.5px]">
+            <div className="flex max-sm:flex-col gap-[50px] max-sm:gap-0">
+              <div className="w-[271.5px] max-lg:w-[50%] max-sm:w-full">
                 <div className="flex justify-between mt-10 pb-[17px] border-b-1 border-b-[#9F9F9F]">
                   <p className="text-[#454545] text-sm">Артикул</p>
                   <p className="text-[16px]">LED LAMPS 81284</p>
@@ -76,7 +76,7 @@ const ProductDetail = () => {
                   <p className="text-[16px]">IP67</p>
                 </div>
               </div>
-              <div className="w-[271.5px]">
+              <div className="w-[271.5px] max-lg:w-[50%] max-sm:w-full">
                 <div className="flex justify-between mt-10 pb-[17px] border-b-1 border-b-[#9F9F9F]">
                   <p className="text-[#454545] text-sm">Мощность, Вт</p>
                   <p className="text-[16px]">16</p>
@@ -97,7 +97,7 @@ const ProductDetail = () => {
             </div>
           </div>
         </div>
-        <ul className="flex gap-15 mt-[78px] mb-[60px] pb-5 border-b border-b-[#9F9F9F]">
+        <ul className="flex gap-15 mt-[78px] mb-[60px] pb-5 border-b border-b-[#9F9F9F] max-sm:overflow-y-hidden">
           <li>
             <NavLink end={true} to={''} className={({ isActive }) => isActive ? 'text-black border-b-2 border-b-black pb-[22px]' : 'text-[#9F9F9F]'}>
               Описание
--- src/pages/product/characteristics/index.tsx
+++ src/pages/product/characteristics/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductCharacteristics = () => {
     return (
-        <div className='flex gap-25'>
+        <div className='flex max-sm:justify-between gap-25'>
             <div className='flex flex-col gap-15 text-[#9F9F9F]'>
                 <p>Вариант установки</p>
                 <p>Напряжение питания</p>
@@ -10,7 +10,7 @@ const ProductCharacteristics = () => {
                 <p>Степень защиты</p>
                 <p>Габариты, мм</p>
             </div>
-            <div className='flex flex-col gap-15'>
+            <div className='flex flex-col gap-15  max-sm:text-right'>
                 <p>Накладной</p>
                 <p>230В AC (50 Гц)</p>
                 <p>0,38 кг</p>
